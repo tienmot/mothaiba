771f0d9fdd770294e428e4ef0a1a89b7b8a055cd
tienmot <106167018+tienmot@users.noreply.github.com>
2022-05-24 22:28:24 +0700

    Add files via upload

---
+++ jscrtp.js
@@ -0,0 +1,13 @@
+
+function blogs(){
+
+    if(document.getElementById('bag-main').style.display === 'none'){
+        document.getElementById('bag-main').style.display = 'block';
+    }else if(document.getElementById('bag-main').style.display === 'block'){
+        document.getElementById('bag-main').style.display = 'none';
+    }
+  
+}
+// function sp1(){
+//     document.getElementById('sp1').src='/sp.html'
+// }
